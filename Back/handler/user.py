5c2ef4fc6a403b44ab702990cba029a0fc4aae66
yuetingpei20181677 <yuetingpei888@gmail.com>
2021-07-16 17:02:17 +0800

    调用员工信息返回json localhost:5000/user/transport

---
+++ Back/handler/user.py
@@ -0,0 +1,15 @@
+from flask import Blueprint,request,Response,jsonify
+
+
+from api.user_api import *
+
+user = Blueprint('user',__name__)
+
+
+
+
+@user.route("/transport")
+def transport():
+    #调用user_api功能方法返回需要的数据
+    data = User_transport()
+    return data
