d13666766f5ee529fd8079b86c7f78f3ff2e40dc
guochenxi <chenxiguo886@gmail.com>
2021-07-20 12:39:59 +0800

    7.20 12:39

---
+++ operation/add_operation.py
@@ -0,0 +1,15 @@
+# 导入数据模型类
+from models.Worker import Worker
+from db_config import db_init as db
+
+
+class Add_Operation():
+
+    def _add(kwargs):
+        # 数据模型类 创建对象
+        me = Worker(**kwargs)
+        # 使用数据库链接对接 在对应表添加一条数据记录
+        db.session.add(me)
+        db.session.commit()
+        data = {'code': 0, 'message': 'success'}
+        return data
