cca4ca64482469cdf79e97fe53b705514160889c
yuetingpei20181677 <yuetingpei888@gmail.com>
2021-07-21 10:31:34 +0800

    upload

--- api/user_api.py
+++ api/user_api.py
@@ -60,3 +60,52 @@ def Account_login(username, password):  # use account table
         result['code'] = -1
         result['message'] = "账户不存在"
     return result
+
+def select_api(username):
+    #查询历史打卡记录
+    user_p = Select_Operation()
+    result_data = user_p._select(username)
+
+    result = Class_To_Data(result_data, user_p.__fields__)
+    return result
+
+def Worker_contact(kwargs):
+    account_operation = Add_Operation()
+    re_data = Add_Operation._add(kwargs)
+    return re_data
+
+
+def select_id_api(userid):
+    user_p = select_id_operation()
+    result_data = user_p._select_id(userid)
+
+    result = Class_To_Data(result_data, user_p.__fields__)
+    return result
+
+def select_name_api(username):
+    user_p = select_name_operation()
+    result_data = user_p._select_name(username)
+
+    result = Class_To_Data(result_data, user_p.__fields__)
+    return result
+
+def select_gender_api(usergender):
+    user_p = select_gender_operation()
+    result_data = user_p._select_gender(usergender)
+
+    result = Class_To_Data(result_data, user_p.__fields__)
+    return result
+
+def select_department_api(userdepartment):
+    user_p = select_department_operation()
+    result_data = user_p._select_department(userdepartment)
+
+    result = Class_To_Data(result_data, user_p.__fields__)
+    return result
+
+def select_contact_api(usercontact):
+    user_p = select_contact_operation()
+    result_data = user_p._select_contact(usercontact)
+
+    result = Class_To_Data(result_data, user_p.__fields__)
+    return result
